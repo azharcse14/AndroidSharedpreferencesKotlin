64952d9fe880b852a711cb73538a2499df47f91b
MD. Azharul Islam <36479711+azharcse14@users.noreply.github.com>
2021-10-10 18:53:42 +0600

    update

--- app/src/main/java/com/azhar/androidsharedpreferenceskotlin/MainActivity.kt
+++ app/src/main/java/com/azhar/androidsharedpreferenceskotlin/MainActivity.kt
@@ -2,10 +2,26 @@
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
-class MainActivity : AppCompatActivity() {
+ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        id = Integer.parseInt(idEtId.text.toString())
+        name = nameEtId.text.toString()
+
+        clearBtnId.setOnClickListener {
+            clearData()
+        }
+
+        loadBtnId.setOnClickListener {
+            loadData()
+        }
+
+        saveBtnId.setOnClickListener{
+            saveData()
+        }
     }
 }
