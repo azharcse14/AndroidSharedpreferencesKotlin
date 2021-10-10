95a7081761ebc3b6dc2fa054bdcaa9a4ae711617
MD. Azharul Islam <36479711+azharcse14@users.noreply.github.com>
2021-10-10 20:07:00 +0600

    update

--- app/src/main/java/com/azhar/androidsharedpreferenceskotlin/MainActivity.kt
+++ app/src/main/java/com/azhar/androidsharedpreferenceskotlin/MainActivity.kt
@@ -3,12 +3,10 @@
 import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
 
  class MainActivity : AppCompatActivity() {
-
-
-     val sharedPreferences = getSharedPreferences("sharedPreps", Context.MODE_PRIVATE)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -28,33 +26,45 @@ import kotlinx.android.synthetic.main.activity_main.*
     }
 
      private fun clearData() {
+         val sharedPreferences = getSharedPreferences("sharedPreps", Context.MODE_PRIVATE)
          val editor = sharedPreferences.edit()
          editor.clear()
          editor.apply()
-
      }
 
      private fun loadData() {
-//         val sharedPreferences = getSharedPreferences("sharedPreps", Context.MODE_PRIVATE)
+         val sharedPreferences = getSharedPreferences("sharedPreps", Context.MODE_PRIVATE)
 
          val id = sharedPreferences.getInt("id_key", 0)
          val name = sharedPreferences.getString("name_key", null)
+         val switch = sharedPreferences.getBoolean("boolean_key", false)
 
          idEtId.setText(id.toString())
          nameEtId.setText(name)
+         switchId.isChecked = switch
 
      }
 
      private fun saveData() {
 
-//         val sharedPreferences = getSharedPreferences("sharedPreps", Context.MODE_PRIVATE)
-         val editor = sharedPreferences.edit()
-
          val id = Integer.parseInt(idEtId.text.toString())
          val name = nameEtId.text.toString()
-         editor.putInt("id_key", id)
-         editor.putString("name_key", name)
-         editor.apply()
+
+         val sharedPreferences = getSharedPreferences("sharedPreps", Context.MODE_PRIVATE)
+         val editor = sharedPreferences.edit()
+
+         editor.apply{
+             putInt("id_key", id)
+             putString("name_key", name)
+             putBoolean("boolean_key", switchId.isChecked)
+         }.apply()
+
+         Toast.makeText(this, "Data Saved", Toast.LENGTH_SHORT).show()
+
+
+//         editor.putInt("id_key", id)
+//         editor.putString("name_key", name)
+//         editor.apply()
 
      }
  }
